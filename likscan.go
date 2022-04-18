91611650577b73c4ddca7a79f65f36e4596c82d5
Мурсалов Станислав Мурсалович <sm_mursalov@betboom.org>
2022-04-18 22:59:06 +0300

    Update

--- likscan.go
+++ likscan.go
@@ -2,13 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
+	"time"
 )
+
+var ChasCH = time.Date(2022, time.April, 18, 12, 0, 0, 0, time.Local)
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Printf("Usage: likscan MAIN SOURCE\n")
+		fmt.Printf("Usage: likscan SOURCE TARGET\n")
 		return
 	}
+	fmt.Printf("LIkScan started\n")
+	src := strings.TrimSuffix(os.Args[1], "/")
+	trg := strings.TrimSuffix(os.Args[2], "/")
+	scanDirectory(src, trg)
+	fmt.Printf("\nLIkScan done\n")
+}
 
+func scanDirectory(src string, trg string) {
+	dirs, err := ioutil.ReadDir(src)
+	if err != nil {
+		return
+	}
+	ms := len(dirs)
+	dirt, err := ioutil.ReadDir(trg)
+	if err != nil {
+		return
+	}
+	mt := len(dirt)
+	ns, nt := 0, 0
+	for ns < ms && nt < mt {
+		elms := dirs[ns]
+		elmt := dirt[nt]
+		names := elms.Name()
+		namet := elmt.Name()
+		if names < namet {
+			ns++
+		} else if names > namet {
+			nt++
+		} else if elms.IsDir() != elmt.IsDir() {
+			ns++
+			nt++
+		} else {
+			target := trg + "/" + namet
+			if elmt.IsDir() {
+				scanDirectory(src+"/"+names, target)
+			}
+			mods := elms.ModTime()
+			modt := elmt.ModTime()
+			if mods.Unix() != modt.Unix() {
+				if modt.Before(ChasCH) {
+					fmt.Printf("%s\n", target)
+					os.Chtimes(target, elms.ModTime(), elms.ModTime())
+				}
+			}
+			ns++
+			nt++
+		}
+	}
 }
